568f214baa02c6b240b8ba7a30a48516ecc291c1
Mohamed_Refaay <bekaasd1111@gmail.com>
2025-06-09 10:09:32 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-}) 
+  base: '/2  export default defineConfig({
+    plugins: [react()],
+    base: '/Anki/', // <-- اسم الريبو الصحيح
+  });  export default defineConfig({
+    plugins: [react()],
+    base: '/Anki/', // <-- اسم الريبو الصحيح
+  });/', // <-- ضع هنا اسم الريبو الفرعي بالضبط
+});
